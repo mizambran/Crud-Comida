e964a06abbb6da98b243ed08b8832772f6e10ac9
Miguel Angel Zambrano <angel245687@gmail.com>
2025-10-22 20:38:29 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+/* PARA COMENZAR UN PROYECTO 
+
+1- abrimos la terminal y creamos la carpeta 
+abrimos vite npm create vite@latest
+projet name = nombre del proyecto 
+
+package name lo mismo que el nombre de proyecto 
+
+luego react 
+
+se comienzan a realizar la instalacion de las dependencias 
+
+npm run dev (para comenzar a correr el proyecto)
+
+vamos a App.jsx
+
+en un gitBash dejar que corra el proyecto y en otro vamos haciendo las instalaciones
+
+eliminamos el index.css
+
+instalamos npm i react-router 
+intalamos npm install react-bootstrap bootstrap */
+
+
 
 import './App.css'
 
